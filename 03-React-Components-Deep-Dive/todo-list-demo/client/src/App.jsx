ee692dac05a1b482387244182d4f00e52f211eaa
Nikolay Marzakov <114495254+nvmarzakov@users.noreply.github.com>
2023-12-02 02:00:29 +0200

    add Header component

--- 03-React-Components-Deep-Dive/todo-list-demo/client/src/App.jsx
+++ 03-React-Components-Deep-Dive/todo-list-demo/client/src/App.jsx
@@ -1,17 +1,11 @@
+import Header from "./components/Header"
 
 
 function App() {
 
     return (
         <div>
-            {/* Navigation header  */}
-            <header className="navigation-header">
-                <span className="navigation-logo">
-                    <img src="./static/images/todo-icon.png" alt="todo-logo" />
-                </span>
-                <span className="spacer"></span>
-                <span className="navigation-description">Todo List</span>
-            </header>
+            <Header />
 
              {/* Main content  */}
             <main className="main">
